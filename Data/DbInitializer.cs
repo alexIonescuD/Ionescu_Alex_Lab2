22621a915c3237c3b423ac39f43ce01e82a1e01d
alexIonescuD <dionescu90@yahoo.com>
2023-10-22 17:31:15 +0300

    Add project files.

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,70 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Ionescu_Alex_Lab2.Models;
+namespace Ionescu_Alex_Lab2.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new
+           LibraryContext(serviceProvider.GetRequiredService
+            <DbContextOptions<LibraryContext>>()))
+            {
+                if (context.Books.Any())
+                {
+                    return; // BD a fost creata anterior
+                }
+                context.Books.AddRange(
+                new Book
+                {
+                    Title = "Baltagul",
+                    Author = new Author
+                    {
+                        FirstName = "Mihail",
+                        LastName = "Sadoveanu"
+                    },
+                    Price=Decimal.Parse("22")},
+               
+                new Book
+                {
+                    Title = "Enigma Otiliei",
+                    Author =new Author
+                    {
+                        FirstName = "George", 
+                        LastName = "Calinescu"
+                    },
+                    Price=Decimal.Parse("18")},
+               
+                new Book
+                {
+                    Title = "Maytrei",
+                    Author = new Author
+                    {
+                        FirstName = "Mircea",
+                        LastName = "Eliade"
+                    },
+                    Price=Decimal.Parse("27")}
+               
+                );
+
+
+                context.Customers.AddRange(
+                new Customer
+                {
+                    Name = "Popescu Marcela",
+                    Adress = "Str. Plopilor, nr. 24",
+                    BirthDate = DateTime.Parse("1979-09-01")
+                },
+                new Customer
+                {
+                    Name = "Mihailescu Cornel",
+                    Adress = "Str. Bucuresti, nr.45,ap. 2",BirthDate=DateTime.Parse("1969 - 07 - 08")}
+               
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
